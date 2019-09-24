de901c16fad598a3885ecfad2bd2f6c8e10b7033
Igor Bakhtin <ibakhtin@icloud.com>
2019-09-24 17:06:02 +0300

    update package.json

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { Greeter } from './Greeter'
 
 const greeting: string = 'Hello'
 const name: string = 'Ivar'
+
 const helloGreeter = new Greeter(greeting)
 
 console.log(helloGreeter.greet(name))
