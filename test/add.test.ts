fdcf28fb450a7398a9b428a8d513f0e5f6b48980
Igor Bakhtin <ibakhtin@icloud.com>
2019-09-28 09:09:00 +0300

    refactor add.test.ts

--- test/add.test.ts
+++ test/add.test.ts
@@ -1,5 +1,17 @@
 import { sum } from '../src/sum'
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
-});
+describe('sum() function test', () => {
+
+  test('sum of two integer numbers 1, 2 is 3', () => {
+    expect(sum(1, 2)).toBe(3)
+  })
+
+  test('sum of two integer numbers 1, 2, 3 is 6', () => {
+    expect(sum(1, 2, 3)).toBe(6)
+  })
+
+  test('sum of two floating point numbers 1.1, 2.2, is 3.3', () => {
+    expect(sum(1.1, 2.2)).toBeCloseTo(3.3)
+  })
+
+})
